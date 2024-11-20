351a57b709adbc7e255101016713975861a97ace
DvlPrSujalPatel <sujal7455@gmail.com>
2024-11-20 21:21:23 +0530

    first commit

---
+++ 01_basics/chai.py
@@ -0,0 +1,3 @@
+from hello_chai import chai
+
+chai("And She is my gf")
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+def chai(n):
+    print(n)
+
+chai("she is a Good Girl")
+chai_one = "She is best"
+chai_two = "She is most beautiful girl for me"
+chai_three = "and I love her"
