b3bd8b4e9e759e0210963284398e505f661b9b29
DvlPrSujalPatel <sujal7455@gmail.com>
2024-12-04 23:24:30 +0530

    first question done

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("You are a Child")
+elif age < 20:
+    print("You are a Teenager")    
+elif age < 60:
+    print("You are a adult")
+else:
+    print("You are a senior")
+
+
+
